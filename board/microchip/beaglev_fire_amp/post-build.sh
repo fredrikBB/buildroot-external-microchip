a505e7b01cf2359806aaf06f0c219cd46401024a
fredrikBB <fredrik.b.bratberg@gmail.com>
2025-10-03 09:54:30 +0200

    Attempt at aligning the AMP build more closely to the icicle one using overlaying and not custom DTS
    This implies removing custom DTS, as we want an approach using overlaying to have a similar setup as possible to the Icicle AMP build. However previos attempts relied on the dt-overlay-mchp package which did not have beaglev-fire support so it did not work. Now we are doing overlaying ourselves.

--- board/microchip/beaglev_fire_amp/post-build.sh
+++ board/microchip/beaglev_fire_amp/post-build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Create boot directory for any additional files if needed
 if ! [ -d "${TARGET_DIR}"/boot ]; then
 	mkdir "${TARGET_DIR}"/boot
 fi
-
-# Copy any device tree overlays to boot directory (optional)
-cp "${BINARIES_DIR}"/*.dtbo "${TARGET_DIR}"/boot 2>/dev/null || true
+if [ -d "${BINARIES_DIR}"/mpfs_beaglev_fire ]; then
+	cp "${BINARIES_DIR}"/mpfs_beaglev_fire/*.dtbo "${TARGET_DIR}"/boot
+else
+	cp "${BINARIES_DIR}"/*.dtbo "${TARGET_DIR}"/boot 2>/dev/null || true
+fi
--- board/microchip/beaglev_fire_amp/post-image.sh
+++ board/microchip/beaglev_fire_amp/post-image.sh
@@ -3,21 +3,40 @@ set -e
 
 GENIMAGE_CFG="$2"
 MKIMAGE="${HOST_DIR}"/bin/mkimage
-
-# Add HOST_DIR/bin to PATH for dtc and other tools
-export PATH="${HOST_DIR}/bin:${PATH}"
+BOARD_DIR="$(pwd)"/"${0%/*}"
 
 pushd "${BINARIES_DIR}"
-# Copy device tree files
 mkdir -p dts
 cp -r microchip/ dts/
-# Compress kernel (must be done before creating FIT image)
-# Remove existing Image.gz if it exists, then compress
-rm -f Image.gz
-gzip -9 -c Image > Image.gz
+mkdir -p mpfs_beaglev_fire
 
-# Copy .its file to binaries directory and generate FIT image
-cp "${BR2_EXTERNAL_MCHP_PATH}/board/microchip/beaglev_fire_amp/mpfs-beaglev-fire-amp.its" ./mpfs-beaglev-fire-amp.its
+# Compile BeagleV-Fire AMP overlay
+DTC="${HOST_DIR}/bin/dtc"
+KERNEL_DIR="${BUILD_DIR}/linux-custom"
+
+# Compile the overlay
+"${HOST_DIR}/bin/cpp" -E -nostdinc \
+	-I"${KERNEL_DIR}/include" \
+	-I"${KERNEL_DIR}/arch/riscv/boot/dts" \
+	-I"${KERNEL_DIR}/arch/riscv/boot/dts/microchip" \
+	-x assembler-with-cpp -undef \
+	-o mpfs_beaglev_fire_amp.pre.dtso \
+	"${BOARD_DIR}/mpfs_beaglev_fire_amp.dtso"
+
+"${DTC}" -@ -Wno-unit_address_vs_reg -I dts -O dtb \
+	-o mpfs_beaglev_fire/mpfs_beaglev_fire_amp.dtbo \
+	mpfs_beaglev_fire_amp.pre.dtso
+
+# Move any other dtbo files to platform directory  
+for file in ./*.dtbo
+do
+  if [ -e "$file" ] && [ "$file" != "./mpfs_beaglev_fire/mpfs_beaglev_fire_amp.dtbo" ]
+  then
+    mv "$file" mpfs_beaglev_fire/
+  fi
+done
+
+gzip -9 Image -c > Image.gz
 "${MKIMAGE}" -f mpfs-beaglev-fire-amp.its mpfs_beaglev_fire_amp.itb
 popd
 support/scripts/genimage.sh -c "${GENIMAGE_CFG}"
