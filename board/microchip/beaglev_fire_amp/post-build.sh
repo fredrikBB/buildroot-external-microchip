e1bbfdb1905a4f9c05f6ca7262c7cdf37858e41f
fredrikBB <fredrik.b.bratberg@gmail.com>
2025-10-06 11:02:35 +0200

    Supress .dtbo file not being present in post-build step
    .dtbo is created in post-image.sh so we supress the error of them not being avaiable in post-build

--- board/microchip/beaglev_fire_amp/post-build.sh
+++ board/microchip/beaglev_fire_amp/post-build.sh
@@ -4,7 +4,7 @@ if ! [ -d "${TARGET_DIR}"/boot ]; then
 	mkdir "${TARGET_DIR}"/boot
 fi
 if [ -d "${BINARIES_DIR}"/mpfs_beaglev_fire ]; then
-	cp "${BINARIES_DIR}"/mpfs_beaglev_fire/*.dtbo "${TARGET_DIR}"/boot
+	cp "${BINARIES_DIR}"/mpfs_beaglev_fire/*.dtbo "${TARGET_DIR}"/boot 2>/dev/null || true
 else
 	cp "${BINARIES_DIR}"/*.dtbo "${TARGET_DIR}"/boot 2>/dev/null || true
 fi
